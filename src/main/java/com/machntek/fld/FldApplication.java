a6b5e63bdbbf39861533146ff21bc78e7407aeff
machntek <machntek@gmail.com>
2021-02-22 22:01:16 +0900

    Initial commit

---
+++ src/main/java/com/machntek/fld/FldApplication.java
@@ -0,0 +1,13 @@
+package com.machntek.fld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/machntek/fld/FldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machntek.fld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
